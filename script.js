2e25a9f287fbff358c859e05d8f012f6c9c47c56
Ambeeee <77481329+Ambeeee@users.noreply.github.com>
2021-05-29 08:43:52 +0200

    Responsive TutteNews 1.0

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ function MenuMobile(){
 }
 
 
-//Responsive News
+//Responsive Pagine
 
 function MenuMobileNGC(){
     var mobileNav = document.getElementById("mobileNav");
@@ -63,5 +63,28 @@ function NewsMobile(){
     else{
         ModsMobile.style.display = "none";
         menu = "close";
+    }
+}
+
+//Responsive pagine interne 
+function MenuMobileNGCi(){
+    var mobileNav = document.getElementById("mobileNav");
+    var iconmenu = document.getElementById("menuIcon");
+    var intestazione = document.getElementById("intestazione")
+    var sfondo1 = document.getElementById("sfondo1")
+    if(menu == "close"){
+        mobileNav.style.display = "flex";
+        intestazione.style.display = "none";
+        sfondo1.style.display = "none";
+        menu="open";
+        iconmenu.src = "../../img/Generale/Chiudi.png";    
+    }
+
+    else{
+        mobileNav.style.display = "none";
+        intestazione.style.display = "block";
+        sfondo1.style.display = "flex";
+        menu = "close"
+        iconmenu.src = "../../img/Generale/IconaMenu1.png" 
     }
 }
